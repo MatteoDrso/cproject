e734daf75f09aebb1f46d0d274164fd762bc7869
matteo <coding.matteo@gmail.com>
2024-03-05 16:48:23 +0100

    bug fix

--- exercise1/my-wc.c
+++ exercise1/my-wc.c
@@ -5,15 +5,18 @@
 int main(int argc, char const *argv[]) {
 
     int c;
-    int countChars = 0, countWords = 0, countLines = 1;
+    int countChars = 0, countWords = 0, countLines = 0;
 
     while(c = getchar(), c != EOF){
         countChars++;
-        if(c == 10) countLines++;
+        if(c == 10){
+            countLines++;
+            countWords++;
+        }
         if(c == 32) countWords++;
     }
 
-    printf("%i, %i, %i\n", countChars, countWords, countLines);
+    printf("%i, %i, %i\n", countChars, countWords, countLines); //result is faulty depending on whether the file has a trailing '\n' or not.
 
     return (0);
 
