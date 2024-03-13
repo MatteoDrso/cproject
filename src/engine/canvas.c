b6da86c8d7a342185e0a5728db9d30c19c8605e6
Moritz Zitzow <moritz.zitzow@gmail.com>
2024-03-13 15:24:50 +0100

    added at and neighbours function

--- src/engine/canvas.c
+++ src/engine/canvas.c
@@ -4,38 +4,88 @@
 
 struct pixel {
   bool visited, wall;
+  int name;
 };
 
-struct canvas {
+typedef struct {
   int width, height;
 
-  struct pixel *start;
-  struct pixel *end;
+  int start_x, start_y;
+  int end_x, end_y;
 
   struct pixel *canv;
-};
+}canvas;
 
-struct canvas init_canvas(int width, int height) {
-  struct canvas c;
+canvas init_canvas(int width, int height, int start_x, int start_y, int end_x, int end_y) {
+  canvas c;
   c.width = width;
   c.height = height;
-  c.canv = malloc(sizeof(struct pixel) * width * height);
+
+  c.start_x = start_x;
+  c.start_y = start_y;
+  c.end_x = end_x;
+  c.end_y = end_y;
+
+  c.canv = malloc(sizeof (struct pixel) * height * width);
 
   for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++) {
       struct pixel p;
       p.visited = false;
       p.wall = true;
-      c.canv[i * j] = p;
+      p.name = 2;
+      c.canv[i * width + j] = p;
     }
   }
 
   return c;
 }
 
+struct pixel *at(canvas *c, int x, int y)
+{
+  return &c->canv[y * c->width + x];
+}
+
+// Returns Pixels in order: North, East, South, West
+void neighbours(canvas *c, int x, int y, struct pixel **n)
+{
+  if (y > 0 && y <= c->height && x >= 0 && x <= c->width) {
+    n[0] = at(c, x, y-1);      
+    } else {
+    n[0] = NULL;
+  }
+  if (y >= 0 && y <= c->height && x >= 0 && x < c->width-1) {
+    n[1] = at(c, x+1, y);
+  } else {
+    n[1] = NULL;
+  }
+  if (y >= 0 && y < c->height-1 && x >= 0 && x <= c->width) {
+    n[2] = at(c, x, y+1);
+  } else {
+    n[2] = NULL;
+  }
+  if (y >= 0 && y <= c->height && x > 0 && x <= c->width) {
+    n[3] = at(c, x-1, y);
+  } else {
+    n[3] = NULL;
+  }
+}
+
+
 int main(void) {
-  struct canvas c = init_canvas(3, 2);
-  printf("%d %d\n",c.canv[0].visited, c.canv[0].wall);
+  canvas c = init_canvas(3, 3, 0, 0, 1, 1);
+
+  printf("Coordinate[2,2] %d\n", at(&c,2,2)->name );
+  printf("visited[2,2] %d\n", at(&c,2,2)->visited );
+  printf("wall[2,2] %d\n", at(&c,2,2)->wall);
+
+  struct pixel *n[4];
+  neighbours(&c, 2, 2, n);
+  for (int i = 0; i < 4; i++) {
+    if (n[i] != NULL) {
+      printf("Nachbar %d: %d\n",i, n[i]->name);
+    }
+  }
 
   return 0;
 }
