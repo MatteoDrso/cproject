94ac26a18fd85d4afaca3190f12891292e48387b
Matteo D'Urso <67522359+MatteoDrso@users.noreply.github.com>
2024-03-14 16:39:05 +0100

    added is_goal helper to algorithms.h

--- src/engine/algorithms/algorithms.h
+++ src/engine/algorithms/algorithms.h
@@ -1,10 +1,17 @@
 #ifndef PATHFIND_ALGORITHMS_H
 #define PATHFIND_ALGORITHMS_H
 #include "../canvas.h"
+#include <stdbool.h>
 
 int dijkstra(canvas *c);
 int a_star(canvas *c);
 int breadth_first_search(canvas *c);
 int depth_first_search(canvas *c);
+
+bool is_goal(canvas *c, int x, int y){
+
+    return (x == c->end_x && y == c->end_y);
+
+}
 
 #endif
