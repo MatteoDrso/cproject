92c2206260b8aab2fdb7ee66e8366ec66f860c82
Matteo D'Urso <67522359+MatteoDrso@users.noreply.github.com>
2024-03-14 15:55:32 +0100

    created algorithms header

---
+++ src/engine/algorithms/algorithms.h
@@ -0,0 +1,10 @@
+#ifndef PATHFIND_ALGORITHMS_H
+#define PATHFIND_ALGORITHMS_H
+#include "../canvas.h"
+
+int dijkstra(canvas *c);
+int a_star(canvas *c);
+int breadth_first_search(canvas *c);
+int depth_first_search(canvas *c);
+
+#endif
