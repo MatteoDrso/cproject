27beef37d448fceaf78ce33c354b6152ac7a2a52
Josua Kaden <115008434+JoKa-1@users.noreply.github.com>
2024-03-16 10:30:51 +0100

    changed prio_queue -> minHeap and added init,push,pop

--- src/engine/data_structures.c
+++ src/engine/data_structures.c
@@ -3,6 +3,7 @@
 #include "canvas.h"
 #include "data_structures.h"
 
+// init
 void stack_init(struct stack *s, int length){
     s->stack_arr = malloc(s->length * sizeof(struct pixel));
     s->length = length;
@@ -16,6 +17,41 @@ void queue_init(struct queue *q, int length){
     q->top = 0;
 }
 
+void minHeap_init(struct minHeap *q, int length){
+    q->length = length;
+    q->queue_arr = malloc(q->length * sizeof(struct pixel));
+    q->bot = 0;
+    q->top = 0;
+}
+
+void minHeap_push(struct minHeap *q, struct pixel p){
+    if(q->top >= q->length){
+        q->top-=q->length;
+    }
+    printf("%d, %d\n", q->top, q->bot);
+    if(q->top == q->bot-1 || (q->top == q->length-1 && q->bot == 0)){
+        printf("Tried to push to a priority queue that is full!\n");
+        return;
+    }
+    q->queue_arr[q->top] = p;
+    q->top++;
+}
+
+struct pixel minHeap_pop(struct minHeap *q){
+    if(q->bot == q->top){
+        printf("Tried to pop a queue that is empty!\n");
+        return;
+    }
+    struct pixel p = q->queue_arr[q->bot];
+    q->bot++;
+    if(q->bot>=q->length){
+        q->bot-=q->length;
+    }
+    return p;
+}
+
+
+// push
 void stack_push(struct stack *s, struct pixel p){
     if(s->top == s->length){
         printf("Tried to push to a stack that is full!\n");
@@ -39,6 +75,7 @@ void queue_push(struct queue *q, struct pixel p){
     q->top++;
 }
 
+//pop
 struct pixel stack_pop(struct stack *s){
     if(s->top<=0){
         printf("Tried to pop a stack that is empty!\n");
