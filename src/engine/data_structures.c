43f3bd2b129ae0bd9e81b7dbe1d31cb821f472a1
Josua Kaden <115008434+JoKa-1@users.noreply.github.com>
2024-03-16 18:30:41 +0100

    added to data_structurs.c

--- src/engine/data_structures.c
+++ src/engine/data_structures.c
@@ -3,6 +3,8 @@
 #include "canvas.h"
 #include "data_structures.h"
 
+int main(void){return 0;}//just for compiling
+
 // init
 void stack_init(struct stack *s, int length){
     s->stack_arr = malloc(s->length * sizeof(struct pixel));
@@ -17,39 +19,12 @@ void queue_init(struct queue *q, int length){
     q->top = 0;
 }
 
-void minHeap_init(struct minHeap *q, int length){
-    q->length = length;
-    q->queue_arr = malloc(q->length * sizeof(struct pixel));
-    q->bot = 0;
-    q->top = 0;
+void min_Heap_init(struct min_Heap *h, int size){
+    h->size = size;
+    h->min_Heap_arr = malloc(h->size * sizeof(node *));//min_Heap_arr is **
+    h->bot = 0;
+    h->top = 0;
 }
-
-void minHeap_push(struct minHeap *q, struct pixel p){
-    if(q->top >= q->length){
-        q->top-=q->length;
-    }
-    printf("%d, %d\n", q->top, q->bot);
-    if(q->top == q->bot-1 || (q->top == q->length-1 && q->bot == 0)){
-        printf("Tried to push to a priority queue that is full!\n");
-        return;
-    }
-    q->queue_arr[q->top] = p;
-    q->top++;
-}
-
-struct pixel minHeap_pop(struct minHeap *q){
-    if(q->bot == q->top){
-        printf("Tried to pop a queue that is empty!\n");
-        return;
-    }
-    struct pixel p = q->queue_arr[q->bot];
-    q->bot++;
-    if(q->bot>=q->length){
-        q->bot-=q->length;
-    }
-    return p;
-}
-
 
 // push
 void stack_push(struct stack *s, struct pixel p){
@@ -75,11 +50,25 @@ void queue_push(struct queue *q, struct pixel p){
     q->top++;
 }
 
+void min_Heap_insert(struct min_Heap *h, pixel *value, int key){
+    if(h->bot == h->size){
+        printf("Tried to push to a full heap!\n");
+        return;
+    }
+    node *n = malloc(sizeof(struct node));
+    n->key = key;
+    n->value = value;
+
+    h->min_Heap_arr[h->bot] = n;
+    bubble_up(h, n);
+    h->bot++;
+}
+
 //pop
 struct pixel stack_pop(struct stack *s){
     if(s->top<=0){
         printf("Tried to pop a stack that is empty!\n");
-        return;
+        abort();
     } 
     s->top--;
     return s->stack_arr[s->top];
@@ -88,7 +77,7 @@ struct pixel stack_pop(struct stack *s){
 struct pixel queue_pop(struct queue *q){
     if(q->bot == q->top){
         printf("Tried to pop a queue that is empty!\n");
-        return;
+        abort();
     }
     struct pixel p = q->queue_arr[q->bot];
     q->bot++;
@@ -97,3 +86,69 @@ struct pixel queue_pop(struct queue *q){
     }
     return p;
 }
+
+struct pixel *min_Heap_pop(struct min_Heap *h){
+     if(h->bot == 0){
+        printf("Tried to push to a full heap!\n");
+        abort();
+    }
+    pixel *p = h->min_Heap_arr[h->top]->value;
+    free(h->min_Heap_arr[h->top]);
+    h->min_Heap_arr[h->top] = h->min_Heap_arr[h->bot]; 
+    // then bubble new root down
+    bubble_down(h, h->min_Heap_arr[h->top]);
+    h->bot--;
+    return p; 
+}
+
+//helper functions for heap
+static void bubble_up(min_Heap *h, node *n){
+    int idx_parent = h->bot;
+    node *parent = h->min_Heap_arr[idx_parent];
+    int idx_child = idx_parent;
+    while(n->key < parent->key){
+        swap(h, idx_parent, idx_child);   
+        idx_child = idx_parent; 
+        idx_parent = get_parent(idx_child);
+        parent = h->min_Heap_arr[idx_parent];
+    }
+}
+
+static void bubble_down(min_Heap *h, node *n){
+    int idx_parent = 0; 
+    int idx_child = 0;
+    while(1){
+        idx_child = get_smallest_child(h, idx_parent);
+        if(idx_child == idx_parent){
+            break;
+        }
+        swap(h, idx_parent, idx_child);   
+        idx_parent = idx_child;
+    }
+}
+
+static int get_smallest_child(min_Heap *h, int index_node){
+    node *leftChild = h->min_Heap_arr[2*index_node+1];
+    node *rightChild = h->min_Heap_arr[2*index_node+2];
+    node *parent = h->min_Heap_arr[index_node];
+    if((parent->key <= leftChild->key) && (parent->key <= rightChild->key)){
+        return index_node;
+    }
+
+    if(rightChild->key < leftChild->key){
+        return (2*index_node+2);
+    }
+    return (2*index_node+1);
+}
+
+static int get_parent(int index_node){
+    return ((index_node-1)/2);
+}
+
+static void swap(min_Heap *h, int index_parent, int index_child){
+    node *temp = h->min_Heap_arr[index_parent];
+    h->min_Heap_arr[index_parent] = h->min_Heap_arr[index_child];
+    h->min_Heap_arr[index_child] = temp;
+}
+
+
--- src/engine/data_structures.h
+++ src/engine/data_structures.h
@@ -18,23 +18,34 @@ typedef struct queue {
 
 }queue;
 
-typedef struct minHeap {
+typedef struct node{
+    pixel *value;//pointer to pixel
+    int key;//distance
+}node;
 
-    struct pixel *minHeap_arr;
-    int length; //has to be initialized to the number of pixel in the canvas (width * height)
+typedef struct min_Heap {
+
+    node **min_Heap_arr;
+    int size; //has to be initialized to the number of pixel in the canvas (width * height)
     int top, bot;
 
-}minHeap;
+}min_Heap;
 
 
 void stack_init(struct stack *s, int length);
 void queue_init(struct queue *q, int length);
-void minHeap_init(struct minHeap *q, int length);
+void min_Heap_init(struct min_Heap *h, int size);
 void stack_push(struct stack *s, struct pixel p);
 void queue_push(struct queue *q, struct pixel p);
-void minHeap_push(struct minHeap *q, struct pixel p);
+void min_Heap_insert(struct min_Heap *h, pixel *p, int key);
 struct pixel stack_pop(struct stack *s);
 struct pixel queue_pop(struct queue *q);
-struct pixel minHeap_pop(struct minHeap *q);
+struct pixel *min_Heap_pop(struct min_Heap *h);
+
+static void swap(min_Heap *h, int index_parent, int index_child);
+static int get_parent(int index_node);
+static int get_smallest_child(min_Heap *h, int index_node); 
+static void bubble_up(min_Heap *h, node *n);
+static void bubble_down(min_Heap *h, node *n);
 
 #endif
