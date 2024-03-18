b5d49a3f6c7e86c44d7a0a0487cd1f662f0ebaba
Matteo D'Urso <67522359+MatteoDrso@users.noreply.github.com>
2024-03-18 19:39:31 +0100

    minor chanes

--- src/gui/file_handler.c
+++ src/gui/file_handler.c
@@ -8,7 +8,8 @@ int read_canvas_from_file(char *file_name){
 
     FILE *f = fopen(file_name, "r");
     char c;
-    canvas *canvas;
+    int width;
+    char *new_canv;
     //canvas->canv = 
 
     // This function reads in the first row of the canvas which has to consists exclusivley of '#' and implies by that the width of the canvas.
@@ -22,7 +23,7 @@ int read_canvas_from_file(char *file_name){
                 return MAX_CANVAS_WIDTH_EXCEEDED;
             }
         } else if (c=='\n') {
-            canvas->width = counter;
+            width = counter;
             break;
         } else {
             return ILLEGAL_BORDER_ELEMENT;
@@ -30,11 +31,18 @@ int read_canvas_from_file(char *file_name){
     }
 
     counter =  0;
-    canvas->canv = NULL;
+    char temp[width];
+    char *curr_row;
 
     for(;;){
-        canvas->canv = reallocarray(canvas->canv, canvas->width, sizeof(pixel))
+        curr_row = fgets(temp, width+2, f);
+        if(curr_row[width+1]!='\0' || curr_row[width]!='\n') return INCONSISTENT_CANVAS_WIDTH;
+
+        new_canv = reallocarray(new_canv, width, sizeof(pixel));
+        if(new_canv == NULL) return CANVAS_FILE_READ_REALLOC_FAILURE; 
+        canvas->canv = new_canv;
         for(int i = 0; i < canvas->width; i++){
+            canvas->canv[canvas->width*counter+i] = fgetc(f);
 
         }
     }
