f3d731c377ce53f0d894ca6ab7c29f1788facc92
Matteo D'Urso <67522359+MatteoDrso@users.noreply.github.com>
2024-03-21 11:27:17 +0100

    minor edit

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 
 int main(int argc, char *argv[]) {
   printf("This is an interactive Pathfinding visualizer.\n");
-  printf("Reading this file: %s", argv[1]);
+  printf("Reading this file: %s\n", argv[1]);
 
   canvas c;
 
@@ -37,6 +37,7 @@ int main(int argc, char *argv[]) {
     case 1: failed = breadth_first_search(&c);
             if (failed) {
               printf("BFS failed!\n");
+              abort();
             } else {
               printf("BFS succeeded!\n");
             }
@@ -44,6 +45,7 @@ int main(int argc, char *argv[]) {
     case 2: failed = depth_first_search(&c);
             if (failed) {
               printf("DFS failed!\n");
+              abort();
             } else {
               printf("DFS succeeded!\n");
             }
@@ -51,6 +53,7 @@ int main(int argc, char *argv[]) {
     case 3: failed = dijkstra(&c);
             if (failed) {
               printf("Dijkstra failed!\n");
+              abort();
             } else {
               printf("Dijkstra succeeded!\n");
             }
@@ -58,6 +61,7 @@ int main(int argc, char *argv[]) {
     case 4: failed = a_star(&c);
             if (failed) {
               printf("A* failed!\n");
+              abort();
             } else {
               printf("A* succeeded!\n");
             }
@@ -66,19 +70,13 @@ int main(int argc, char *argv[]) {
             break;
   }
 
-  draw_path_on_canvas(&c);
+    draw_path_on_canvas(&c);
 
-  failed = write_canvas_to_file("found_canv.txt", &c);
-  if (failed) {
-    printf("canv_to_file failed. Errorcode: %d", failed);
-  }
+    printf("Canvas: \n");
+    print_canvas(&c);
+    print_path(&c);
 
-  printf("Canvas: \n");
-  print_canvas(&c);
-
-  print_path(&c);
-
-  free_canvas(&c);
+    free_canvas(&c);
 
   return 0;
 }
